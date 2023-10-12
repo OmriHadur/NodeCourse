4147f84f83ec8edfa1608f133e8bf9f41457e814
Omri Hadur <omri.hadur@gmail.com>
2023-10-12 21:10:47 +0300

    .

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export default ({ req }) => {
     if (typeof window === 'undefined') {
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            //baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http:/146.190.178.62',
             headers: req.headers
         })
     } else {
